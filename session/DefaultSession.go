debb5cf534c68e6814ef0ed2cca804cbfbf79ae7
Suzuki Takeo <takeo@stko.info>
2020-04-16 18:06:22 +0900

    外部依存を削除

--- session/DefaultSession.go
+++ session/DefaultSession.go
@@ -1,10 +1,9 @@
 package session
 
 import (
-	"github.com/BambooTuna/quest-market/backend/json"
-	"github.com/BambooTuna/quest-market/backend/settings"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -13,36 +12,40 @@ type DefaultSession struct {
 	Settings SessionSettings
 }
 
-func (s DefaultSession) SetSession(f func(*gin.Context) (*string, error)) func(*gin.Context) {
+func GenerateUUID() (string, error) {
+	uuidObj, err := uuid.NewUUID()
+	data := []byte("wnw8olzvmjp0x6j7ur8vafs4jltjabi0")
+	uuidObj2 := uuid.NewMD5(uuidObj, data)
+	return uuidObj2.String(), err
+}
+
+func (s DefaultSession) SetSession(f func(*gin.Context) *string) func(*gin.Context) {
 	return func(c *gin.Context) {
-		plainToken, err := f(c)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, json.ErrorMessageJson{Message: err.Error()})
+		plainToken := f(c)
+		if plainToken == nil {
 			return
 		}
-		id, err := settings.GenerateUUID()
+		id, err := GenerateUUID()
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusInternalServerError)
 			return
 		}
-
 		token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), &jwt.StandardClaims{Id: id})
-
 		SignedToken, err := token.SignedString([]byte(s.Settings.Secret))
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusInternalServerError)
 			return
 		}
 
 		if err := s.Dao.Store(id, *plainToken, s.Settings.ExpirationDate); err != nil {
-			c.JSON(http.StatusInternalServerError, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusInternalServerError)
 			return
 		}
 		c.Header(s.Settings.SetAuthHeaderName, SignedToken)
 	}
 }
 
-func (s DefaultSession) RequiredSession(f func(*gin.Context, *string)) func(*gin.Context) {
+func (s DefaultSession) RequiredSession(f func(*gin.Context, string)) func(*gin.Context) {
 	return func(c *gin.Context) {
 		claimsId := s.ParseClaimsId(c)
 		if claimsId == "" {
@@ -50,10 +53,10 @@ func (s DefaultSession) RequiredSession(f func(*gin.Context, *string)) func(*gin
 		}
 		token, err := s.Dao.Find(claimsId)
 		if err != nil {
-			c.JSON(http.StatusForbidden, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusForbidden)
 			return
 		}
-		f(c, token)
+		f(c, *token)
 	}
 }
 
@@ -69,12 +72,12 @@ func (s DefaultSession) OptionalRequiredSession(f func(*gin.Context, *string)) f
 			return []byte(s.Settings.Secret), nil
 		})
 		if err != nil {
-			c.JSON(http.StatusForbidden, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusForbidden)
 			return
 		}
 		token, err := s.Dao.Find(claims.Id)
 		if err != nil {
-			c.JSON(http.StatusForbidden, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusForbidden)
 			return
 		}
 		f(c, token)
@@ -82,14 +85,14 @@ func (s DefaultSession) OptionalRequiredSession(f func(*gin.Context, *string)) f
 }
 
 func (s DefaultSession) InvalidateSession(f func(*gin.Context)) func(*gin.Context) {
-	return s.RequiredSession(func(c *gin.Context, i *string) {
+	return s.RequiredSession(func(c *gin.Context, i string) {
 		claimsId := s.ParseClaimsId(c)
 		if claimsId == "" {
 			return
 		}
 
 		if err := s.Dao.Remove(claimsId); err != nil {
-			c.JSON(http.StatusInternalServerError, json.ErrorMessageJson{Message: err.Error()})
+			c.Status(http.StatusInternalServerError)
 			return
 		}
 		f(c)
@@ -99,7 +102,7 @@ func (s DefaultSession) InvalidateSession(f func(*gin.Context)) func(*gin.Contex
 func (s DefaultSession) ParseClaimsId(c *gin.Context) string {
 	tokenString := c.GetHeader(s.Settings.AuthHeaderName)
 	if tokenString == "" {
-		c.JSON(http.StatusForbidden, json.ErrorMessageJson{Message: "please set session token to header"})
+		c.Status(http.StatusForbidden)
 		return ""
 	}
 	var claims jwt.StandardClaims
@@ -107,7 +110,7 @@ func (s DefaultSession) ParseClaimsId(c *gin.Context) string {
 		return []byte(s.Settings.Secret), nil
 	})
 	if err != nil {
-		c.JSON(http.StatusForbidden, json.ErrorMessageJson{Message: err.Error()})
+		c.Status(http.StatusForbidden)
 		return ""
 	}
 	return claims.Id
