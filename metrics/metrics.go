0ca1949a240008b2e0c99258e0889aba0ad36533
Suzuki Takeo <takeo@stko.info>
2020-06-09 17:11:22 +0900

    add label

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -14,53 +14,57 @@ func CreateMetrics(namespace string) Metrics {
 	return Metrics{namespace: namespace, counter: make(map[string]prometheus.Counter), gauge: make(map[string]prometheus.Gauge)}
 }
 
-func (m *Metrics) Counter(name string) prometheus.Counter {
+func (m *Metrics) Counter(name string, label map[string]string) prometheus.Counter {
 	if value, ok := m.counter[name]; ok {
 		return value
 	}
 	c := prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: m.namespace,
-		Name:      name + "_Counter",
-		Help:      name + " help",
+		Namespace:   m.namespace,
+		Name:        name + "_Counter",
+		Help:        name + " help",
+		ConstLabels: label,
 	})
 	m.counter[name] = c
 	return c
 }
 
-func (m Metrics) Gauge(name string) prometheus.Gauge {
+func (m Metrics) Gauge(name string, label map[string]string) prometheus.Gauge {
 	if value, ok := m.gauge[name]; ok {
 		return value
 	}
 	g := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: m.namespace,
-		Name:      name + "_Gauge",
-		Help:      name + " help",
+		Namespace:   m.namespace,
+		Name:        name + "_Gauge",
+		Help:        name + " help",
+		ConstLabels: label,
 	})
 	m.gauge[name] = g
 	return g
 }
 
-func (m Metrics) Histogram(name string) prometheus.Histogram {
+func (m Metrics) Histogram(name string, label map[string]string) prometheus.Histogram {
 	if value, ok := m.histogram[name]; ok {
 		return value
 	}
 	h := prometheus.NewHistogram(prometheus.HistogramOpts{
-		Namespace: m.namespace,
-		Name:      name + "_Histogram",
-		Help:      name + " help",
+		Namespace:   m.namespace,
+		Name:        name + "_Histogram",
+		Help:        name + " help",
+		ConstLabels: label,
 	})
 	m.histogram[name] = h
 	return h
 }
 
-func (m Metrics) Summary(name string) prometheus.Summary {
+func (m Metrics) Summary(name string, label map[string]string) prometheus.Summary {
 	if value, ok := m.summary[name]; ok {
 		return value
 	}
 	s := prometheus.NewSummary(prometheus.SummaryOpts{
-		Namespace: m.namespace,
-		Name:      name + "_Summary",
-		Help:      name + " help",
+		Namespace:   m.namespace,
+		Name:        name + "_Summary",
+		Help:        name + " help",
+		ConstLabels: label,
 	})
 	m.summary[name] = s
 	return s
