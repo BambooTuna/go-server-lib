4f637794c4ff5ae800c4d41dee398df2e68c6a80
Suzuki Takeo <takeo@stko.info>
2020-06-16 14:34:40 +0900

    fix package name

--- connection/redis/redis.go
+++ connection/redis/redis.go
@@ -3,15 +3,15 @@ package redis
 import (
 	"fmt"
 	"github.com/BambooTuna/quest-market/backend/settings"
-	"github.com/go-redis/redis"
+	goredis "github.com/go-redis/redis"
 )
 
-func RedisConnection(db int) *redis.Client {
+func RedisConnection(db int) *goredis.Client {
 	redisAddr := fmt.Sprintf("%s:%s",
 		settings.FetchEnvValue("REDIS_HOST", "127.0.0.1"),
 		settings.FetchEnvValue("REDIS_PORT", "6379"),
 	)
-	connection := redis.NewClient(&redis.Options{
+	connection := goredis.NewClient(&goredis.Options{
 		Addr:     redisAddr,
 		Password: settings.FetchEnvValue("REDIS_PASSWORD", ""),
 		DB:       db,
