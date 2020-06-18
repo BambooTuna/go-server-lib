129046a87142558db20f43c5e5b7270302da43e5
Suzuki Takeo <takeo@stko.info>
2020-06-19 05:53:52 +0900

    replace

--- connection/redis/redis.go
+++ connection/redis/redis.go
@@ -2,18 +2,18 @@ package redis
 
 import (
 	"fmt"
-	"github.com/BambooTuna/quest-market/backend/settings"
+	"github.com/BambooTuna/go-server-lib/config"
 	goredis "github.com/go-redis/redis"
 )
 
 func RedisConnection(db int) *goredis.Client {
 	redisAddr := fmt.Sprintf("%s:%s",
-		settings.FetchEnvValue("REDIS_HOST", "127.0.0.1"),
-		settings.FetchEnvValue("REDIS_PORT", "6379"),
+		config.GetEnvString("REDIS_HOST", "127.0.0.1"),
+		config.GetEnvString("REDIS_PORT", "6379"),
 	)
 	connection := goredis.NewClient(&goredis.Options{
 		Addr:     redisAddr,
-		Password: settings.FetchEnvValue("REDIS_PASSWORD", ""),
+		Password: config.GetEnvString("REDIS_PASSWORD", ""),
 		DB:       db,
 	})
 	return connection
